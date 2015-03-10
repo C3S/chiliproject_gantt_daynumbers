e7a18df17999993155b922fe41ce9e06d62cd2ae
Alexander Blum <alexander.blum@c3s.cc>
2015-03-10 23:04:47 +0100

    init

---
+++ init.rb
@@ -0,0 +1,13 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_gantt_daynumbers do
+  name 'Quiet Parenttickets'
+  author 'Alexander Blum'
+  description 'This plugin for ChiliProject causes gantt to show daynumbers instead of weeknumbers'
+  version '0.1'
+  author_url 'mailto:alexander.blum@c3s.cc'
+  url 'https://github.com/C3S/chiliproject_gantt_daynumbers'
+end
